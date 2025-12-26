5013867afce5d1115ded4f91b2eaa4687454a039
abi <abisheela88@gmail.com>
2025-12-26 10:00:12 +0530

    update env

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: false,
   //apiBaseUrl: 'http://localhost:5219/api'
-  apiBaseUrl: 'http://3.109.123.208:8080/api'
+ // apiBaseUrl: 'http://3.109.123.208:8080/api'
+ apiBaseUrl: 'https://smartbillpro.duckdns.org/api'
+
 
 
 
